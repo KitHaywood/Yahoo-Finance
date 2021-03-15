4ecbeb2ff8ffb4cc0fa0384a147c2b78bbf84bc5
kit haywood <kithaywood@RokuExpress-265.hall>
2021-03-15 20:52:18 +0000

    First Attempts

---
+++ yahoo-fin-test.py
@@ -0,0 +1,15 @@
+import yahoo_finance as yf 
+
+# Get all tickers
+class Tickers():
+    def __init__(self):
+        super().__init__()
+        self.name = name
+        
+
+
+
+#
+
+if __name__=="__main__":
+    print(yahoo_finance.__file__)
