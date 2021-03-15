5bf63f22207bb75d45e68efb35c5bbc9417cb156
kit haywood <kithaywood@RokuExpress-265.hall>
2021-03-15 21:12:28 +0000

    Get Ticker Codes

--- yahoo-fin-test.py
+++ yahoo-fin-test.py
@@ -1,15 +1,28 @@
 import yahoo_finance as yf 
+import requests
+import pandas as pd 
 
-# Get all tickers
-class Tickers():
+class TickerTable():
     def __init__(self):
         super().__init__()
-        self.name = name
-        
+    
+    def getTable(self):
+        self.url = 'https://en.wikipedia.org/wiki/FTSE_100_Index#cite_note-13'
+        self.html = requests.get(self.url).content
+        self.df_list = pd.read_html(self.html)[3]
+        return self.df_list
+    
+class getTickerData():
+    def __init__(self):
+        super().__init__()
 
+    def getData(self,ticker):
+        self.ticker = ticker
 
-
-#
 
 if __name__=="__main__":
-    print(yahoo_finance.__file__)
+    data = TickerTable().getTable()
+    tickers = list(data['EPIC'])
+    print(tickers)
+    
+    
